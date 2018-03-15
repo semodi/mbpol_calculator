f3eba628c961d5cd8d183a89090dc86bb98f5ec6
Sebastian Dick <sebastian-dick@gmx.net>
2018-03-15 10:32:03 -0400

    Update utils.py
    decreased precision in pdb file to avoid read errors

--- mbpol_calculator/utils.py
+++ mbpol_calculator/utils.py
@@ -28,14 +28,14 @@ def coords_to_pdb(coords, n_mol):
             for molecule in range(n_mol):
                 for l, loh in zip(labels,labeloh):
                     pdbfile.write('HETATM{:5d}  '.format(atm_cnt) + l + 'HOH A{:4d}    '.format(mol_cnt))
-                    pdbfile.write('{:7.4f} {:7.4f} {:7.4f}  1.00  0.00'.format(coords[cnt,0],
+                    pdbfile.write('{:7.3f} {:7.3f} {:7.3f}  1.00  0.00'.format(coords[cnt,0],
                                                                               coords[cnt,1],
                                                                               coords[cnt,2]) + ' '*11 + loh + '  \n')
                     cnt += 1
                     atm_cnt += 1
                     
                 pdbfile.write('HETATM{:5d}  '.format(atm_cnt) + 'M   ' + 'HOH A{:4d}    '.format(mol_cnt))
-                pdbfile.write('{:7.4f} {:7.4f} {:7.4f}  1.00  0.00 \t \t \n'.format(0,0,0))
+                pdbfile.write('{:7.3f} {:7.3f} {:7.3f}  1.00  0.00 \t \t \n'.format(0,0,0))
                 atm_cnt += 1
                 mol_cnt += 1
 
